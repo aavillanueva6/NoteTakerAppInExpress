b51f082745abc47bb2013284dc7990f4b853d1be
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-04-29 12:21:25 -0700

    adds nodemon for dev, beginning of setting up server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// define server
+const express = require('express');
+const app = express();
+const PORT = 3001;
+const path = require('path');
+
+//
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
