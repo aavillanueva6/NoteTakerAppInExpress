0c4e4e15031bdc5646f350a4374dae369a1513fa
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-01 18:20:32 -0700

    adds uuid package, api routes, removes built uuid module

--- server.js
+++ server.js
@@ -5,16 +5,34 @@ const PORT = 3001;
 
 // add required modules
 const path = require('path');
-const uuid = require('./helpers/uuid.js');
+const { v4: uuidv4 } = require('uuid');
 
-//
+// define middleware for public files
 app.use(express.static('public'));
+
+// define database
+const notes = require('./db/db.json');
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+  console.log('get request received');
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  console.log('post request received');
+  res.send('');
+});
+
+app.delete('/api/notes', (req, res) => {
+  console.log('delete request received');
+  res.send('');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  console.log(uuid());
+  console.log(uuidv4());
 });
