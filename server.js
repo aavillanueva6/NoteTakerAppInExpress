942d596d06b631365b584bbd21d3f6d3721cbfe4
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-01 20:55:20 -0700

    cleans up comments, removes console.log statements that were added during development

--- server.js
+++ server.js
@@ -15,18 +15,18 @@ app.use(express.json());
 // define database
 const notes = require('./db/db.json');
 
+// handles route for the /notes url.  loads the notes.html page.
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+// GET method for /api/notes url.  Responds with the notes stored in the db.json file.
 app.get('/api/notes', (req, res) => {
-  console.log('get request received');
   res.json(notes);
 });
 
+// POST method for /api/notes url.  Adds a new note object to the db.  Uses uuid npm package to create a unique id for the note.
 app.post('/api/notes', (req, res) => {
-  console.log('post request received');
-
   const { title, text } = req.body;
   const newNote = {
     title,
@@ -34,52 +34,64 @@ app.post('/api/notes', (req, res) => {
     id: uuidv4(),
   };
 
+  // define response for api to send back
   const response = {
     status: 'success',
     body: newNote,
   };
-  // console.log(response);
-  console.log(notes);
-  console.log(newNote);
-  // Convert the data to a string so we can save it
+
+  // add the newNote onto the list.
   notes.push(newNote);
-  console.log(notes);
-  const noteString = JSON.stringify(notes);
 
-  // Write the string to a file
-  fs.writeFile(`./db/db.json`, noteString, (err) =>
-    err ? console.error(err) : console.log(`Note has been written to JSON file`)
-  );
-
-  res.status(201).json(response);
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-  console.log('delete request received');
-  const id = req.params.id;
-
-  let noteIndex;
-  notes.forEach((element) => {
-    if (element.id === id) {
-      noteIndex = notes.indexOf(element);
-    }
-  });
-
-  const deletedTitle = notes[noteIndex].title;
-
-  notes.splice(noteIndex, 1);
-  res.status(200).send(`Delete request for note: ${deletedTitle} successful`);
+  // Convert the data to a string so we can save it
   const noteString = JSON.stringify(notes);
 
   // Write the string to a file
   fs.writeFile(`./db/db.json`, noteString, (err) =>
     err
       ? console.error(err)
-      : console.log(`Note has been removed from JSON file`)
+      : console.info(`Note (${newNote.title}) has been written to JSON file`)
   );
+
+  // send response to close api call
+  res.status(201).json(response);
+});
+
+// DELETE method for /api/notes/:id url.  Deletes specified note object from the db.
+app.delete('/api/notes/:id', (req, res) => {
+  // pulls the note id from the request url
+  const id = req.params.id;
+
+  // initialize a variable to define the index of the specified note.
+  let noteIndex;
+
+  // loop through notes array to find the element that matches the id of the note that was selected by the user.
+  notes.forEach((element) => {
+    if (element.id === id) {
+      noteIndex = notes.indexOf(element);
+    }
+  });
+
+  // pulls the title of the note to delete (to be used in response / console.info)
+  const deletedTitle = notes[noteIndex].title;
+
+  // removes the identified note from the array.
+  notes.splice(noteIndex, 1);
+
+  // Convert the data to a string so we can save it
+  const noteString = JSON.stringify(notes);
+
+  // Write the string to a file
+  fs.writeFile(`./db/db.json`, noteString, (err) =>
+    err
+      ? console.error(err)
+      : console.info(`Note (${deletedTitle}) has been removed from JSON file`)
+  );
+
+  // send response to close api call
+  res.status(200).send(`Delete request for note: ${deletedTitle} successful`);
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  console.log(uuidv4());
 });
