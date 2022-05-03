72074753d86a6c1b34ea96e39e093dbc807e74f9
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-02 21:15:01 -0700

    updates PORT for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // define server
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 5000;
 
 // add required modules
 const path = require('path');
